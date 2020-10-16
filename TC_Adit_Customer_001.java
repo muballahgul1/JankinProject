1afc5dbf7c6ffd475d93b90afa83d4b3a3978996
Muballahgul <gul_lisha@yahoo.com>
2020-10-15 22:01:09 -0400

    added search customer and modified adit_customer

--- TC_Adit_Customer_001.java
+++ TC_Adit_Customer_001.java
@@ -1,2 +1,8 @@
 //This is new test case 
 //It will Review by other team member
+
+
+
+//This code added newly..
+//This code added newly..
+//This code added newly..
---
+++ src/test/java/Base/TC_Customer_Search.java
@@ -0,0 +1,5 @@
+package Base;
+
+public class TC_Customer_Search {
+	//This is Customer Search file
+}
